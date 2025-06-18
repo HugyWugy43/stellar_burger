99ddb59803c6b45ac6f458fd1540b4e0ed09fa65
oumisa <rodionovmax28@gmail.com>
2025-06-18 03:24:29 +0300

    Добавлен роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  useNavigate
+} from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
 import {
   ConstructorPage,
   Feed,
@@ -20,12 +26,18 @@ const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
   return isAuthenticated ? element : <Login />;
 };
 
-const App = () => (
-  
-  <Router>
-    <div className={styles.app}>
-      <AppHeader />
-      <Routes>
+const AppRoutes = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const state = location.state as { backgroundLocation?: Location };
+  const backgroundLocation = state?.backgroundLocation;
+
+  const closeModal = () => navigate(-1);
+
+  return (
+    <>
+      <Routes location={backgroundLocation || location}>
         <Route path='/' element={<ConstructorPage />} />
         <Route path='/feed' element={<Feed />} />
         <Route path='/login' element={<ProtectedRoute element={<Login />} />} />
@@ -51,6 +63,54 @@ const App = () => (
         />
         <Route path='*' element={<NotFound404 />} />
       </Routes>
+
+      {backgroundLocation && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal
+                title={`#${location.pathname.match(/\d+/)}`}
+                onClose={closeModal}
+              >
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title={`Детали ингредиента`} onClose={closeModal}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <ProtectedRoute
+                element={
+                  <Modal
+                    title={`#${location.pathname.match(/\d+/)}`}
+                    onClose={closeModal}
+                  >
+                    <OrderInfo />
+                  </Modal>
+                }
+              />
+            }
+          />
+        </Routes>
+      )}
+    </>
+  );
+};
+
+const App = () => (
+  <Router>
+    <div className={styles.app}>
+      <AppHeader />
+      <AppRoutes />
     </div>
   </Router>
 );
